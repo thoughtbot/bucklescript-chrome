2282b96bc58fa9a9cac31d54b6b39d22b591b96d
Sid Raval <sid@thoughtbot.com>
2017-07-29 14:45:38 -0400

    Add types for detail object for `getAuthToken`

--- src/chrome.ml
+++ src/chrome.ml
@@ -1,6 +1,20 @@
-type chrome
+class type _accountInfo = object
+  method id : string
+end [@bs]
 
-external getAuthToken : 'a Js.t -> (string -> unit) -> unit = "getAuthToken" [@@bs.scope "chrome", "identity"] [@@bs.val]
+type accountInfo = _accountInfo Js.t
+
+class type _getAuthTokenOptions = object
+  method interactive : Js.boolean
+  method scopes : string list
+  method account : accountInfo
+end [@bs]
+
+type getAuthTokenOptions = _getAuthTokenOptions Js.t
+
+external getAuthToken : getAuthTokenOptions -> (string -> unit) -> unit = "getAuthToken" [@@bs.scope "chrome", "identity"] [@@bs.val]
+external mkAuthOptions : ?interactive:Js.boolean -> ?scopes:string list -> ?account:accountInfo -> unit -> getAuthTokenOptions = "" [@@bs.obj]
+external mkAccountInfo : ?id:string -> unit -> accountInfo = "" [@@bs.obj]
 
 type window
 external window : window = "window" [@@bs.val]
